fdc46029d45f428d170aa9fd682addf23495c4fb
Shams Ali <shams.ali0216@gmail.com>
2021-08-19 01:10:12 -0400

    complete timer

--- src/pages/Timer/index.js
+++ src/pages/Timer/index.js
@@ -1,10 +1,8 @@
 // Globals
 import "./styles.scss";
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 // Components
 import { Button } from "components/Button";
-
 // Sub-component
 function Expired() {
   return (
@@ -17,10 +15,39 @@ function Expired() {
 
 // Component
 function Timer() {
-  // Hooks - state
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState('00');
+  const [isActive, setIsActive] = useState(false);
 
-  // TODO: implement counter...
+  const startTimer = () => {
+    setCounter('60');
+    setIsActive(true);
+  }
+  const reset = () => {
+    setCounter('60');
+    setIsActive(false);
+  }
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        if(+counter === 0){
+          setIsActive(false);
+        } else {
+          setCounter(counter => {
+            if(+counter < 10){
+              return `0${+counter - 1}`;
+            } else {
+              return `${+counter - 1}`;
+            }
+          });
+        }
+      }, 1000);
+    } else if (!isActive && +counter !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, counter]);
+
 
   // Render
   return (
@@ -28,12 +55,12 @@ function Timer() {
       <h1>Timer</h1>
 
       <div className="aura-page-content">
-        <div className="aura-timer-clock">0:00</div>
-        {counter <= 0 ? <Expired /> : null}
+        <div className="aura-timer-clock">{counter === '60' ? `1:00` : `0:${counter}`}</div>
+        {+counter <= 0 ? <Expired /> : null}
 
         <div className="aura-timer-buttons">
-          <Button>Start</Button>
-          <Button>Reset</Button>
+          <Button onClick={startTimer}>Start</Button>
+          <Button onClick={reset}>Reset</Button>
         </div>
       </div>
     </div>
